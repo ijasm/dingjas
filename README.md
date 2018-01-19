c2021c81c58c5dc8bf3d42f34b57f0e4f3252cbe
Jasm <jasmine_cheah@tech.gov.sg>
2018-01-19 15:30:11 +0800

    using ts koa boilerplate

---
+++ README.md
@@ -0,0 +1,35 @@
+# node-ts-boilerplate
+
+Minimal boilerplate for Node.js typescript projects
+
+Included:
+
+- Node.js
+- Typescript
+- Koa
+- Koa Router
+- Nodemon
+- Supertest
+
+Cloning:
+
+```bash
+git clone git@github.com:ivpusic/node-ts-boilerplate.git
+```
+
+## Usage
+
+Start server (port 3000):
+
+```bash
+npm start
+```
+
+Run tests:
+
+```bash
+npm test
+```
+
+## License
+*MIT*
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,11 @@
-import * as KoaRouter from "koa-router";
-import * as Koa from "koa";
+import app from './app';
 
-import { RegisterRoutes } from "./routes";
-
-// import all the controllers for tsoa to generate routes
-import "./controller.ts";
-
-const PORT = "1002";
-
-const app = new Koa();
-const router = new KoaRouter();
-// RegisterRoutes(router);
-
-router.get("/helloworld", (ctx, next) => {
-    ctx.body = "hello world";
-});
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-async function start() {
-    await app.listen(PORT, async () => {
-        console.log(`MOL Intranet started on port ${PORT}`);
-    });
+async function main() {
+  const port = 3000;
+  app.listen(port);
+  console.info(`app running on port ${port}`);
 }
 
-start();
+main().catch((e) => {
+  console.log('error detected', e);
+});
