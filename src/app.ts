2ada3cf5321a39b7df3bde9ae5a23096b674b9a6
Hoon Ding Yi <hoon_ding_yi@tech.gov.sg>
2018-01-26 15:03:24 +0800

    add git-clone lib to clone repo.

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,28 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
+import * as services from "./services";
 
 const app = new Koa();
 
 // global error handler
 app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    console.error('Error detected', err);
+	try {
+		await next();
+	} catch (err) {
+		console.error('Error detected', err);
 
-    ctx.status = err.status || 500;
-    ctx.body = {
-      error: err.message,
-    };
-  }
+		ctx.status = err.status || 500;
+		ctx.body = {
+			error: err.message,
+		};
+	}
 });
 
 const router = new Router();
 
 router.post('/github/hook', async (ctx) => {
-  console.log("push detected: ", ctx.req);
-  ctx.body = "ok";
+	console.log("push detected: ", ctx.req);
+	ctx.body = "ok";
 });
 
 app.use(router.routes());
