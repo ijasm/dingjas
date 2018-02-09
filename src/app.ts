b31d9057fe0c0517554e3688a77c65a91d712c57
Hoon Ding Yi <hoon_ding_yi@tech.gov.sg>
2018-02-09 16:35:26 +0800

    add run unit tests for trey's beatrice, and JSON parse body.

--- src/app.ts
+++ src/app.ts
@@ -2,32 +2,33 @@ import * as Koa from 'koa';
 import * as Router from 'koa-router';
 import * as koaBody from "koa-body";
 import { cloneGitRepo } from "./GitServices";
+import { runUnitTests } from './ci-services';
 
 const app = new Koa();
 
 // global error handler
 app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    console.error('Error detected', err);
+	try {
+		await next();
+	} catch (err) {
+		console.error('Error detected', err);
 
-    ctx.status = err.status || 500;
-    ctx.body = {
-      error: err.message,
-    };
-  }
+		ctx.status = err.status || 500;
+		ctx.body = {
+			error: err.message,
+		};
+	}
 });
 
 const router = new Router();
 
 router.post('/github/hook', koaBody(), (ctx) => {
-  console.log("push detected: ", ctx.request.body);
-  const payload = ctx.request.body;
-  const repoURL = payload.repository.url;
-  cloneGitRepo(repoURL);
+	console.log("push detected: ", ctx.request.body);
+	const payload = JSON.parse(ctx.request.body.payload);
+	const repoURL = payload.repository.url;
+	cloneGitRepo(repoURL);
 
-  ctx.body = JSON.stringify(ctx.request.body);
+	ctx.body = JSON.stringify(ctx.request.body);
 });
 
 app.use(router.routes());
