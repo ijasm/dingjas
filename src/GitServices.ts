01e0b23f83db82569e4f1bfd576dfd5e6ebfe40c
Hoon Ding Yi <hoon_ding_yi@tech.gov.sg>
2018-02-23 15:23:31 +0800

    remove repo dir before cloning.

--- src/GitServices.ts
+++ src/GitServices.ts
@@ -1,9 +1,11 @@
 const clone = require("git-clone");
 import { runUnitTests } from "./ci-services";
+import * as shell from "shelljs";
 
 export const cloneGitRepo = (repoURL: string) => {
 	console.log(__dirname);
 	const dir = __dirname + "/../../repos/";
+	shell.exec(`rm -rf ${dir}`);
 	clone(repoURL, dir, null, () => {
 		console.log(`cloned git repo: "${repoURL}" to ${dir}`);
 		runUnitTests(repoURL);
